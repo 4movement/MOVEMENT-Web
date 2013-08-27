94556c4d507cdf4e25be788e01fe0ddf2bf1b47f
Jui Cheng Lai <skylovely2005@hotmail.com>
2013-08-27 23:22:42 +0800

    New

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  
+  $(".link_project").click(function(){
+	$(".content_toggle").load("project_intro.html");
+	$(".fadeout").fadeOut('slow');
+  }); 
+  
+  $(".link_to_do").click(function(){
+	$(".content_toggle").load("to_do_list.html");
+	$(".fadeout").fadeOut('slow');
+  });
+  
+  $(".link_idea").click(function(){
+	$(".content_toggle").load("idea_pool.html");
+	$(".fadeout").fadeOut('slow');
+  });
+  
+  $(".link_team").click(function(){
+	$(".content_toggle").load("team_member.html");
+	$(".fadeout").fadeOut('slow');
+  });
+  
+  	$(".link_contact").click(function(){
+	$(".content_toggle").load("contact_us.html");
+	$(".fadeout").fadeOut('slow');
+  });
+  //Ajax技術
+});
+
